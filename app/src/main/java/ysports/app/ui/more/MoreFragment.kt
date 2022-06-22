1810368f1159871e3f601371740f360d6e73422b
Mubashir P A <mubashirpa2002@gmail.com>
2022-06-22 15:27:40 +0530

    Removed some comments

--- app/src/main/java/ysports/app/ui/more/MoreFragment.kt
+++ app/src/main/java/ysports/app/ui/more/MoreFragment.kt
@@ -12,9 +12,6 @@ import ysports.app.databinding.FragmentMoreBinding
 class MoreFragment : Fragment() {
 
     private var _binding: FragmentMoreBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
