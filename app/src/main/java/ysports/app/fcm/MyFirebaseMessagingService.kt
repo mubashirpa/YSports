22743aa5dd4ce379348d5b8de0f377de6e5af294
mubashir-pa <mubashirpa2002@gmail.com>
2022-07-30 13:07:45 +0530

    Notification functions moved to NotificationUtil class

--- app/src/main/java/ysports/app/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/ysports/app/fcm/MyFirebaseMessagingService.kt
@@ -1,14 +1,9 @@
 package ysports.app.fcm
 
 import android.annotation.SuppressLint
-import android.app.Notification
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -16,8 +11,11 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import ysports.app.MainActivity
 import ysports.app.R
+import ysports.app.util.NotificationUtil
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private var notificationId = 100
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From: ${remoteMessage.from}")
@@ -41,55 +39,37 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun createNotificationChannel() {
-        val channelId: String = getString(R.string.default_notification_channel_id)
-        val channelName: CharSequence = getString(R.string.default_notification_channel_name)
-        val channelDescription = getString(R.string.default_notification_channel_description)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = channelDescription
-            channel.enableLights(true)
-            channel.setShowBadge(true)
-            channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-            notificationManager.createNotificationChannel(channel)
+            val channelId: String = getString(R.string.default_notification_channel_id)
+            val channelName: String = getString(R.string.default_notification_channel_name)
+            val channelDescription = getString(R.string.default_notification_channel_description)
+            NotificationUtil(this).createNotificationChannel(channelName, channelDescription, channelId, NotificationManager.IMPORTANCE_HIGH)
         }
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
     private fun sendNotification(messageTitle: String?, messageBody: String?) {
+        notificationId++
         val channelId: String = getString(R.string.default_notification_channel_id)
-        var notificationId = 100
-        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
         val pendingIntent: PendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         } else {
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         }
 
-        val notificationBuilder: NotificationCompat.Builder =
-            NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle(messageTitle)
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setContentIntent(pendingIntent)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            notificationBuilder.priority = NotificationManager.IMPORTANCE_DEFAULT
+            NotificationUtil(this).createNotification(
+                channelId, messageTitle!!, messageBody!!, NotificationManager.IMPORTANCE_HIGH, pendingIntent, notificationId
+            )
+        } else {
+            NotificationUtil(this).createNotification(
+                channelId, messageTitle!!, messageBody!!, NotificationCompat.PRIORITY_HIGH, pendingIntent, notificationId
+            )
         }
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // Since android Oreo notification channel is needed.
-        createNotificationChannel()
-
-        notificationId++
-        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
     companion object {
