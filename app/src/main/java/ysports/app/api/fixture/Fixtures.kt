6540c2864f097c5ccbf0127c2b035c2c17df98d5
mubashir-pa <mubashirpa2002@gmail.com>
2022-08-01 12:07:04 +0530

    Variable name changes

--- app/src/main/java/ysports/app/api/fixture/Fixtures.kt
+++ app/src/main/java/ysports/app/api/fixture/Fixtures.kt
@@ -3,15 +3,15 @@ package ysports.app.api.fixture
 import com.google.gson.annotations.SerializedName
 
 data class Fixtures(
-    @SerializedName("homeTeam") val homeTeam: String?,
-    @SerializedName("homeTeamScore") val homeTeamScore: String?,
-    @SerializedName("homeTeamLogo") val homeTeamLogo: String?,
-    @SerializedName("awayTeam") val awayTeam: String?,
-    @SerializedName("awayTeamScore") val awayTeamScore: String?,
-    @SerializedName("awayTeamLogo") val awayTeamLogo: String?,
-    @SerializedName("leagueName") val leagueName: String?,
-    @SerializedName("matchDate") val matchDate: String?,
-    @SerializedName("matchTime") val matchTime: String?,
+    @SerializedName("home_team") val homeTeam: String?,
+    @SerializedName("home_team_score") val homeTeamScore: String?,
+    @SerializedName("home_team_logo") val homeTeamLogo: String?,
+    @SerializedName("away_team") val awayTeam: String?,
+    @SerializedName("away_team_score") val awayTeamScore: String?,
+    @SerializedName("away_team_logo") val awayTeamLogo: String?,
+    @SerializedName("league_name") val leagueName: String?,
+    @SerializedName("match_date") val matchDate: String?,
+    @SerializedName("match_time") val matchTime: String?,
     @SerializedName("media") val media: ArrayList<Media>?,
     @SerializedName("url") val url: String?
 )
