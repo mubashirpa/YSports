02ec988f8eb8bf3087e2b2f09c5efffcfaf6d4eb
mubashir-pa <mubashirpa2002@gmail.com>
2022-08-01 12:08:09 +0530

    Variable name changed

--- app/src/main/java/ysports/app/api/leagues/Leagues.kt
+++ app/src/main/java/ysports/app/api/leagues/Leagues.kt
@@ -3,9 +3,7 @@ package ysports.app.api.leagues
 import com.google.gson.annotations.SerializedName
 
 data class Leagues(
-    @SerializedName("league_id") val id: String?,
     @SerializedName("league_title") val title: String?,
-    @SerializedName("league_title_alternate") val alternateTitle: String?,
-    @SerializedName("club_logo") val logo: String?,
+    @SerializedName("league_logo") val logo: String?,
     @SerializedName("url") val url: String?
 )
