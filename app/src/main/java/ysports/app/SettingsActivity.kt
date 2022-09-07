4fe0e61624183b2e9b9eb44dc29b92ac6b65480d
Mubashir P A <mubashirpa2002@gmail.com>
2022-09-07 18:21:39 +0530

    Added more settings items

--- app/src/main/java/ysports/app/SettingsActivity.kt
+++ app/src/main/java/ysports/app/SettingsActivity.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import ysports.app.databinding.ActivitySettingsBinding
+import ysports.app.util.FileUtil
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -39,17 +42,26 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
+        private val fileUtil = FileUtil()
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             val notificationsPreference: Preference? = findPreference("notifications")
             val themePreference: ListPreference? = findPreference("theme")
+            val cachePreference: Preference? = findPreference("clear_cache")
+            val aboutPreference: Preference? = findPreference("about")
+            val licensesPreference: Preference? = findPreference("licenses")
+            val versionPreference: Preference? = findPreference("version")
 
             when (themePreference?.value) {
                 "light" -> themePreference.summary = getString(R.string.summary_theme_light)
                 "dark" -> themePreference.summary = getString(R.string.summary_theme_dark)
                 else -> themePreference?.summary = getString(R.string.summary_theme_system_default)
             }
+            cachePreference?.summary = getCacheSize()
+            versionPreference?.summary = getString(R.string.summary_version, BuildConfig.VERSION_NAME)
 
             notificationsPreference?.setOnPreferenceClickListener {
                 val intent = Intent().apply {
@@ -86,6 +98,37 @@ class SettingsActivity : AppCompatActivity() {
                 }
                 true
             }
+
+            cachePreference?.setOnPreferenceClickListener {
+                val cacheDeleted = fileUtil.deleteDir(context?.cacheDir)
+                val externalCacheDeleted = fileUtil.deleteDir(context?.externalCacheDir)
+                if (cacheDeleted || externalCacheDeleted) {
+                    Toast.makeText(context, getString(R.string.cache_cleared), Toast.LENGTH_LONG).show()
+                }
+                it.summary = getCacheSize()
+                true
+            }
+
+            aboutPreference?.setOnPreferenceClickListener {
+                val intent = Intent(context, BrowserActivity::class.java).apply {
+                    putExtra("WEB_URL", getString(R.string.url_about))
+                }
+                startActivity(intent)
+                true
+            }
+
+            licensesPreference?.setOnPreferenceClickListener {
+                startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+                true
+            }
+        }
+
+        private fun getCacheSize() : String {
+            var size: Long = 0
+            size += fileUtil.getDirSize(context?.cacheDir)
+            size += fileUtil.getDirSize(context?.externalCacheDir)
+            val cacheSize = if (size > 0) (size shr 20).toDouble() else 0
+            return "$cacheSize MB"
         }
     }
 }
