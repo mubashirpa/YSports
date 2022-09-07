5e23064c58118df862af8d0ca550f95bddbaed49
Mubashir P A <mubashirpa2002@gmail.com>
2022-09-07 18:17:21 +0530

    Initial commit

---
+++ app/src/main/java/ysports/app/connectivity/ConnectivityObserver.kt
@@ -0,0 +1,12 @@
+package ysports.app.connectivity
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectivityObserver {
+
+    fun observe() : Flow<Status>
+
+    enum class Status {
+        Available, Unavailable, Losing, Lost
+    }
+}
