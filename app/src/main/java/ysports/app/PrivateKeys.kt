e48e718ab0ecc8e299c6b2a88ad77c92f73cfc35
Mubashir P A <mubashirpa2002@gmail.com>
2022-09-23 20:56:48 +0530

    Optimised

--- app/src/main/java/ysports/app/PrivateKeys.kt
+++ app/src/main/java/ysports/app/PrivateKeys.kt
@@ -6,8 +6,8 @@ class PrivateKeys {
      * A native method that is implemented by the 'ysports' native library,
      * which is packaged with this application.
      */
-    external fun newsApiKey(): String
-    external fun youtubeApiKey(): String
+    external fun matchesUrlPath(): String
+    external fun leaguesUrlPath(): String
 
     companion object {
         // Used to load the 'ysports' library on application startup.
