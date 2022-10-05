2779bce6d86cf7cd5081ef2d2687ef0fd0524361
Mubashir P A <mubashirpa2002@gmail.com>
2022-10-05 21:36:19 +0530

    Removed

--- app/src/main/java/ysports/app/api/fixture/FixtureResponse.kt
+++
@@ -1,7 +0,0 @@
-package ysports.app.api.fixture
-
-import com.google.gson.annotations.SerializedName
-
-data class FixtureResponse(
-    @SerializedName("fixtures") val fixtures: ArrayList<Fixtures>?
-)
