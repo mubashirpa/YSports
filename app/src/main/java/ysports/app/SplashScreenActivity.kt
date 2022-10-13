f7086714fed8bc96cfa5feab78ab950d4c80aac6
Mubashir P A <mubashirpa2002@gmail.com>
2022-10-13 17:22:40 +0530

    Moved dark mode to Application class

--- app/src/main/java/ysports/app/SplashScreenActivity.kt
+++ app/src/main/java/ysports/app/SplashScreenActivity.kt
@@ -1,38 +1,13 @@
 package ysports.app
 
 import android.content.Intent
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.preference.PreferenceManager
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
-
-        val settingsPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        when (settingsPreferences.getString("theme", "system_default")) {
-            "light" -> {
-                Log.d("SplashScreenActivity", "Light")
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-            "dark" -> {
-                Log.d("SplashScreenActivity", "Dark")
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }
-            else -> {
-                Log.d("SplashScreenActivity", "Auto")
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
-                }
-            }
-        }
-
         super.onCreate(savedInstanceState)
 
         // Keep the splash screen visible for this activity
