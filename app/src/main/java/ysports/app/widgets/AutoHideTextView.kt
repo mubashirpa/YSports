93a98015ddd0d6095e6faef3beffce564d5138c9
Mubashir P A <mubashirpa2002@gmail.com>
2022-10-30 10:06:35 +0530

    Formatted code Fixed auto hide not working Added custom attributes

--- app/src/main/java/ysports/app/widgets/AutoHideTextView.kt
+++ app/src/main/java/ysports/app/widgets/AutoHideTextView.kt
@@ -5,21 +5,49 @@ import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import ysports.app.R
 
 class AutoHideTextView : AppCompatTextView {
 
     private var mHandler: Handler? = null
     private var runnable: Runnable? = null
+    private var hideMillis: Long = 1000
 
     constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, android.R.attr.textViewStyle)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?) : this(
+        context,
+        attrs,
+        android.R.attr.textViewStyle
+    )
 
-    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        val typedArray = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.AutoHideTextView,
+            defStyleAttr,
+            0
+        )
+        try {
+            hideMillis = typedArray.getInt(R.styleable.AutoHideTextView_hide_millis, 1000).toLong()
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    override fun onTextChanged(
+        text: CharSequence?,
+        start: Int,
+        lengthBefore: Int,
+        lengthAfter: Int
+    ) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter)
-        if (mHandler == null) Handler(Looper.getMainLooper())
-        if (runnable == null) Runnable { this.visibility = GONE }
+        if (mHandler == null) mHandler = Handler(Looper.getMainLooper())
+        if (runnable == null) runnable = Runnable { this.visibility = GONE }
         mHandler?.removeCallbacks(runnable!!)
-        mHandler?.postDelayed(runnable!!, 1000)
+        mHandler?.postDelayed(runnable!!, hideMillis)
     }
 }
